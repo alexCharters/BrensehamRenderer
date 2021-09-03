44f37db4e94563aa39d0fb093c5740e843f392f8
Alex Charters <alexcharters45@gmail.com>
2021-09-02 18:32:35 -0600

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
+
+#include"shaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+
+
+GLfloat vertices[] = {
+	-0.5f,	 -0.5f,	 0.0f,		1.0f, 0.0f, 0.0f,
+	-0.5f,	 0.5f,	 0.0f,		0.0f, 1.0f, 0.0f,
+	0.5f,	 0.5f,	 0.0f,		0.0f, 0.0f, 1.0f,
+	0.5f,	 -0.5f,	 0.0f,		1.0f, 1.0f, 1.0f,
+	
+};
+
+GLuint indices[] = {
+	0,2,1,
+	0,3,2
+};
+
+
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Renderer", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	Shader shaderProgram("default.vert", "default.frag");
+
+	VAO VAO1;
+	VAO1.Bind();
+
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	int widthImg, heightImg, numColCh;
+	unsigned char* bytes = stbi_load("texture.jpg", &widthImg, &heightImg, &numColCh, 0);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		shaderProgram.Activate();
+
+		glUniform1f(uniID, 0.0f);
+
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
