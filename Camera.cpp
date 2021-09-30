67743371ee5eb775dc3b924c0752f0865d86dc3a
Alex Charters <alexcharters45@gmail.com>
2021-09-30 11:27:16 -0600

    finished with light types

---
+++ Camera.cpp
@@ -0,0 +1,63 @@
+#include"Camera.h"
+
+
+
+Camera::Camera(int width, int height, glm::vec3 position)
+{
+	Camera::width = width;
+	Camera::height = height;
+	Position = position;
+}
+
+void Camera::updateMatrix(float FOVdeg, float nearPlane, float farPlane) {
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+
+	//Up = up;
+
+	view = glm::lookAt(Position, Position + Orientation, Up);
+	projection = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
+	
+	cameraMatrix = projection * view;  
+}
+
+void Camera::Matrix(Shader& shader, const char* uniform) {
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(cameraMatrix));
+}
+
+void Camera::Inputs(GLFWwindow* window)
+{
+	// Handles key inputs
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		Position += speed * Orientation;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		Position += speed * -glm::normalize(glm::cross(Orientation, Up));
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		Position += speed * -Orientation;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		Position += speed * glm::normalize(glm::cross(Orientation, Up));
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		Position += speed * Up;
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+	{
+		Position += speed * -Up;
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+	{
+		speed = 0.004f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
+	{
+		speed = 0.001f;
+	}
+}
--- VBO.h
+++ VBO.h
@@ -1,17 +1,22 @@
 #ifndef VBO_CLASS_H
 #define VBO_CLASS_H
 
-#include <glad/glad.h>
+#include<glad/glad.h>
 
-class VBO {
-	public:
-		GLuint ID;
+class VBO
+{
+public:
+	// Reference ID of the Vertex Buffer Object
+	GLuint ID;
+	// Constructor that generates a Vertex Buffer Object and links it to vertices
+	VBO(GLfloat* vertices, GLsizeiptr size);
 
-		VBO(GLfloat* indices, GLsizeiptr size);
-
-		void Bind();
-		void Unbind();
-		void Delete();
+	// Binds the VBO
+	void Bind();
+	// Unbinds the VBO
+	void Unbind();
+	// Deletes the VBO
+	void Delete();
 };
 
 #endif
